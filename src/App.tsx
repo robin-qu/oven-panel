b67968d40210b54a5598e17ed4e7bd06cd023c5a
Robin Qu <hongbinqu@microsoft.com>
2022-07-17 18:48:34 +0800

    add rc-knobs lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import { Knob, Arc } from 'rc-knob'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>title</div>
+        {/* <div>
+          <Knob 
+            size={100}  
+            angleOffset={220} 
+            angleRange={280}
+            min={0}
+            max={100}
+            onChange={value => console.log(value)}
+          >
+            <Arc 
+              arcWidth={5}
+              color="#FC5A96"
+              background="#180094"
+            />
+          </Knob>
+        </div> */}
       </header>
     </div>
   );
